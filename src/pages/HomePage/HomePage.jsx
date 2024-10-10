84f2e4cd05422e790e1699e26e97abc42b50d4c9
pilipchu <114575426+pilipchu@users.noreply.github.com>
2024-10-10 10:38:17 +0300

    home page

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,16 +1,20 @@
-import { NavLink } from 'react-router-dom'
-import css from './HomePage.module.css'
+import { NavLink } from "react-router-dom";
+import css from "./HomePage.module.css";
 
-export default function HomePage (){
-   return(
-      <main>
-         <div className={css.conteiner}>
-            <div>
-            <h1>Campers of your dreams</h1>
-            <p>You can find everything you want in our catalog</p>
-            </div>
-            <NavLink to='/catalog'>View Now</NavLink>
-         </div>
-      </main>
-   )
+export default function HomePage() {
+  return (
+    <main>
+      <div className={css.conteiner}>
+        <div className={css.text}>
+          <h1 className={css.title}>Campers of your dreams</h1>
+          <p className={css.text_item}>
+            You can find everything you want in our catalog
+          </p>
+        </div>
+        <NavLink className={css.link} to="/catalog">
+          View Now
+        </NavLink>
+      </div>
+    </main>
+  );
 }
